e2e4fb534bc5464bf5e8289abb34d9b25a0cf7dd
Postdie <fernandezdiego845@gmail.com>
2022-10-21 00:39:57 -0500

    Login 6

--- js/registro.js
+++ js/registro.js
@@ -3,7 +3,7 @@ const nombre = document.getElementsById('name');
 const email = document.getElementsById('email');
 const user = document.getElementsById('user');
 const pass = document.getElementsById('pass');
-const formulario = document.getElementById('formRegistro');
+const formulario = document.getElementsById('formRegistro');
 
 formulario.addEventListener('submit', registro);
 
@@ -15,5 +15,22 @@ function registro(e);{
     let userVal = user.value;
     let passVal = pass.value;
 
-    if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == '');
+    if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == ''){
+        return;
+}
+
+const usuario = {
+    nombre: nombreVal,
+    email: emailVal,
+    user: userVal,
+    pass: passVal
+}
+
+localStorage.setItem('usuario', JSON.stringify(usuario));
+
+nombre.value = '';
+email.value = '';
+user.value = '';
+pass.value = '';
+
 }
