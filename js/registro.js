61aaea1f1632409e358c9385f7782a9909ebed8b
Postdie <fernandezdiego845@gmail.com>
2022-10-21 00:59:24 -0500

    Login 7

--- js/registro.js
+++ js/registro.js
@@ -16,6 +16,7 @@ function registro(e);{
     let passVal = pass.value;
 
     if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == ''){
+        creaMensaje('Verifica tus campos!', 'danger');
         return;
 }
 
@@ -33,4 +34,17 @@ email.value = '';
 user.value = '';
 pass.value = '';
 
+creaMensaje('Usuario Registrado!', 'success');
+
+}
+
+function creaMensaje(texto, tipo){
+    const nuevoElemento = document.createElement('div');
+    nuevoElemento.innerText = texto;
+    nuevoElemento.classList.add('alert', 'alert-' + tipo);
+    const divMensaje = document.getelementsById('mensaje');
+    divMensaje.appendChild(nuevoElemento);
+    setTimeout(function(){
+        nuevoElemento.remove();
+    }, 4000);
 }
